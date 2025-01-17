c5e2223d8ab0693d523d418f9303cd52a4de1533
rafid <rafidahmed6548@gmail.com>
2025-01-17 23:55:21 +0600

    adding solutions

--- Programming with MATLAB/Assignment1.m
+++ Programming with MATLAB/Assignment1.m
@@ -1,3 +1,52 @@
+
+
+%%
+%2(b)
+syms t c
+T(t)=-2*t^3+9*t^2-12*t+25
+a=0;
+b=6;
+tan_slope(c)=diff(T(c),c)
+avg_slope=(T(6)-T(0))/(6-0)
+solv=vpa(solve(tan_slope==avg_slope,c),4)
+for i=1:length(solv)
+    if solv(i)>=a && solv(i)<=b
+        
+        mvc=vpa(solv(i),4);
+        disp(mvc)
+    
+    end
+end
+
+display(['c:',num2str(3.791)])
+%secant line equation :
+values=linspace(a,b,1000)
+sec_line=T(a)+avg_slope*(values-a)
+%tangent line equation:
+tan_line=T(mvc)+tan_slope(mvc)*(values-mvc)
+VALUES=T(values);
+%plotting
+
+figure
+hold on;
+
+plot(values,VALUES,'c')
+plot(values,sec_line,'r')
+plot(values,tan_line,'k')
+scatter(mvc,T(mvc),'ko','filled')
+scatter([a,b],[T(a),T(b)],'yo','filled')
+title('Temperature modelling')
+xlabel('Time in hours')
+ylabel('Temperature')
+l = legend('function','Secant line','Tangent line');
+title(l,'Tracking the temperature changes')
+grid on
+hold off
+avg_temp=(T(6)-T(0))/(6-0)
+% interpreting the result:
+fprintf('The point c represents the time where the rate of temperature change equals the average rate of change over the interval.\nThis can help monitor when rapid changes in temperature occur,aiding in maintaining stable storage.\nThis will help tweaking temperature when it is mandatory')
+
+
 %%
 %(3[i])
 syms x y
