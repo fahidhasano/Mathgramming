e8fef95e293fdd9f28af9045db159c8fddedaea1
rafid <rafidahmed6548@gmail.com>
2025-04-10 19:26:48 +0600

    assignment3 completed

--- Programming with MATLAB/Assignment3.m
+++ Programming with MATLAB/Assignment3.m
@@ -38,10 +38,11 @@ hold on
 y0=3;
 [x,y]=ode45(@(x,y) (exp(-x)-y).*(exp(-x)+2+y), xspan, y0);
 hold on
-plot(x,y,'r-','linewidth',2)
+plot(x,y,'b-','linewidth',2)
 hold on
 plot(2,3,'o','markersize',10,'markerfacecolor','g')
 axis tight
+legend('direction field','solution curve for y(2)=-1','(x,y)=(2,-1)','solution curve for y(2)=3','(x,y)=(2,3)')
 %%
 %2(b)
 fprintf('Plotting a direction field helps because the differential equation\nis quite complicated and nonlinear.An exact analytical solution might be\n very hard or impossible to find. By plotting the direction field, we can\n visually understand the behavior of solutions — for example, where solutions are\n increasing, decreasing, or\n staying constant. It gives a qualitative idea of the system''s\n dynamics without solving it exactly.')
@@ -62,11 +63,24 @@ xspan=[2 10]
 y0=3
 [x,y] = ode45(@odefun, xspan, y0);
 hold on
-plot(x,y,'r-','linewidth',2)
+plot(x,y,'k-','linewidth',2)
 hold on
 plot(2,3,'o','markersize',10,'markerfacecolor','g')
 axis tight
-hold on
+hold off
+%%
+%3
+clc;clear all
+syms Q(t) Q0 k
+general_solution=dsolve(diff(Q,t)==-k*Q,Q0==Q(0))
+half_life_eq=subs(general_solution,t,1500)==Q0/2
+k_solution= solve(half_life_eq,k)
+fprintf('Decay constant k is: %f\n',k_solution)
+Q_sol_k=subs(general_solution,k,k_solution)
+time_3q0by4=solve(Q_sol_k==3*Q0/4,t)
+fprintf('The time(years) of when \nthe original amount be reduced to 3Q0/4 is: %f years\n',time_3q0by4)
+Q_2000=subs(Q_sol_k,t,2000)
+fprintf('Amount left after 2000 years: %s\n',Q_2000)
 %%
 %4
 clc;clear all;
@@ -83,6 +97,31 @@ syms  y(x)
 Dy = diff(y);
 p=dsolve(diff(y, 2) == 4*diff(y,1)-3*y+9*x.^2+4, y(0) == 1, Dy(0) == 8)
 ezplot(p,[0, 20])
-
-
-
+%%
+%5
+clc;clear all;close all;format compact
+syms t
+K=106;
+r=0.71;
+P0=0.25*K;
+P(t)=(K.*P0)./(P0+(K-P0).*exp(-r.*t));
+fprintf('Biomass after 2years is: %f kg\n',P(2))
+tau=solve(P(t)==0.75*K,t)
+fprintf('Time for P(t)=0.75K is: %f years\n',tau)
+t1=solve(diff(P,t,2)==0,t)
+fprintf('Time for population growing fastest is: %f years\n',t1)
+t = linspace(0, 10, 100); % 0 to 10 years
+figure;
+plot(t,P(t),'k','linewidth',3)
+grid on
+xlabel('time(years)')
+ylabel('biomass(kg)')
+title('Logistic model of the halibut population growth')
+hold on
+plot(2,P(2),'o','markersize',10,'markerfacecolor','b')
+hold on
+plot(tau,0.75*K,'o','markersize',10,'markerfacecolor','g')
+hold on
+plot(t1,K/2,'o','markersize',10,'markerfacecolor','r')
+legend('graph of population P(t)','biomass after 2 years point','0.75K point','fastest growth')
+hold off
